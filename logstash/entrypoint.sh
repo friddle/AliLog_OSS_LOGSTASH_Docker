4bcdc2fa9110453f1c10feae4a410c84a73da644
friddle <friddle@qq.com>
2016-09-07 10:34:48 +0800

    change entrypoint output

--- logstash/entrypoint.sh
+++ logstash/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Wait for the Elasticsearch container to be ready before starting Kibana.
+# Wait for the Elasticsearch container to be ready before starting Logstash.
 echo "Stalling for Elasticsearch"
 while true; do
     nc -q 1 elasticsearch 9200 2>/dev/null && break
